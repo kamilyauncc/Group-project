59d7cbdb2a333de57e1a29326a32262ad2bef7e0
newtr <newtrevord@gmail.com>
2024-07-29 09:39:40 -0400

    Added customer, guest_order, menu_item, and promotion to the model_loader

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, guest_order, promotion, menu_item, guest_order
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,7 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    guest_order.Base.metadata.create_all(engine)
+    promotion.Base.metadata.create_all(engine)
+    menu_item.Base.metadata.create_all(engine)
+    guest_order.Base.metadata.create_all(engine)
