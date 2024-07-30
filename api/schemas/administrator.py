3a39923ceb1a36df49050ae117ba19d44a1056d6
kamilyauncc <kkarimo1@uncc.edu>
2024-07-29 22:29:58 -0400

    Update administrator.py

--- api/schemas/administrator.py
+++ api/schemas/administrator.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+class Administrator(Base):
+    username: str
+    password: str
+    email: str
+    role: str
+
+class Administrator (Base):
+   username: Optional[str] = None
+   password: Optional[str] = None
+   email: Optional[str] = None 
+   role: Optional[str] = None
+
 
